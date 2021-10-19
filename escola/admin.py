5cf1d28c86b8fcfdf66c037a2cd312b8a74148ac
Jorge <jorge.henrique.moreira.santana@gmail.com>
2021-10-19 10:17:43 -0300

    Added tables to admin

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from escola.models import Aluno, Curso
 
-# Register your models here.
+
+class TabelaAlunos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'documento', 'tipo_documento', 'data_nascimento')
+    list_display_links = ('id', 'nome')
+    search_fields = ('nome', 'documento')
+    list_per_page = 20
+
+
+class TabelaCursos(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'codigo', 'descricao', 'nivel')
+    list_display_links = ('id', 'nome', 'codigo')
+    search_fields = ('nome', 'codigo')
+    list_per_page = 20
+
+
+admin.site.register(Aluno, TabelaAlunos)
+admin.site.register(Curso, TabelaCursos)
--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-&wb^_#^g$*+r2%$9p(eb+8&-wwr3vu^f!k1w_dlq&^2kb)(r_z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -86,6 +85,14 @@ DATABASES = {
     }
 }
 
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'sqlite3',
+    }
+}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -128,5 +135,3 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-ALLOWED_HOSTS = ['*']
